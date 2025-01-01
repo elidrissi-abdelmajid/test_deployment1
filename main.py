2da938be1788d40c874d833ad4f057ed84388296
elidrissi <ms.abdelmjigmail.com>
2025-01-01 12:36:34 +0100

    update yaml file

--- main.py
+++ main.py
@@ -5,5 +5,8 @@ app = FastAPI()
 @app.get("/")
 def func():
     return {
-        "message": "hello world"
+        "message": "hello world",
+        "AI" : "comment tu vas mon frère ? ",
+        "message" : "je suis trés bien hmdullah et toi  comment allez vous ?",
+        "AI": "super, merci"
     }
