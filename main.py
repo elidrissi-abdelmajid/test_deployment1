3784e0d98dc48e0c363ce28f8a1f888ff109a05b
elidrissi <ms.abdelmjigmail.com>
2025-01-02 11:02:00 +0100

    update the main file

--- main.py
+++ main.py
@@ -8,5 +8,7 @@ def func():
         "message1": "hello world",
         "AI1" : "comment tu vas mon frère ? ",
         "message2" : "je suis trés bien hmdullah et toi  comment allez vous ?",
-        "AI2": "super, merci"
+        "AI2": "super, merci",
+        "message3":"iwa mzyan assidi",
+        "AI3": "layhfdk abro"
     }
